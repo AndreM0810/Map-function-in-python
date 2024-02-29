425a2effdfa1e4ab128f2634edf56267e9f46332
André Morais <65051274+AndreM0810@users.noreply.github.com>
2024-02-29 09:43:40 -0300

    map_function.py

--- map_function.py
+++ map_function.py
@@ -1,4 +1,4 @@
-#a function that maps a value (var) that is in a initial range (min -> max) to another range (n_min -> n_max)
+#A function that maps a value (var) that is in a initial range (min -> max) to another range (n_min -> n_max)
 
 #this function can be useful for a lot of things. Normalizing a vector, for example.
 
@@ -8,9 +8,10 @@ def map(var, min, max, n_min, n_max):
 	mapped_value = ((var - min) / (max -min)) * (n_max - n_min) + n_min
 	return mapped_value
 
-#One exemplo of the many things it can do.
+#One exemple of the many things it can do.
 #Given the value 10 wich goes between 0 and 100 map it to go between 0 and 10.
-#The function should return 0.1s
+#(in this case, value is a constant, but it can be used with variables as stated before)
+#The function should return 0.1
 value = 10
 
 new_x = map(value, 0, 100, 0, 10)
