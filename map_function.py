42b409dba967a8c67b2cff18d16038315e82c311
André Morais <65051274+AndreM0810@users.noreply.github.com>
2024-03-15 06:31:31 -0300

    Update map_function.py

--- map_function.py
+++ map_function.py
@@ -1,19 +1,10 @@
-#A function that maps a value (var) that is in a initial range (min -> max) to another range (n_min -> n_max)
+#A function that maps a value (var) tha's in a initial range (min -> max) to another range (n_min -> n_max)
+#inspired by the map() function in the p5.js framework: https://p5js.org/reference/#/p5/map
 
-#this function can be useful for a lot of things. Normalizing a vector, for example.
-
-def map(var, min, max, n_min, n_max):
+def map(var, min, max, new_min, new_max):
+	#raises value error if the number to be mapped is outside the given initial range
 	if var < min or var > max:
 		raise ValueError("Variable outside initial range")
-	mapped_value = ((var - min) / (max -min)) * (n_max - n_min) + n_min
+	#Re-maps a number (var) from one range to another
+	mapped_value = ((var - min) / (max -min)) * (new_max - new_min) + new_min
 	return mapped_value
-
-#One exemple of the many things it can do.
-#Given the value 10 wich goes between 0 and 100 map it to go between 0 and 10.
-#(in this case, value is a constant, but it can be used with variables as stated before)
-#The function should return 0.1
-value = 10
-
-new_x = map(value, 0, 100, 0, 10)
-
-print(new_x)	
